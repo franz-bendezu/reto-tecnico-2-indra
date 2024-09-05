c313fbb42ba41be18c597fa99c6a2ee796a4a5d0
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-09-05 08:23:07 +0000

    feat: add tag Docker images and push to Docker Hub

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # Set environment variables
 FRONTEND_IMAGE="frontend:latest"
 BACKEND_IMAGE="backend:latest"
+DOCKER_HUB_USERNAME="franzant"
 NAMESPACE="app"  # Optional, you can deploy in the default namespace
 K8S_DIR="./k8s"  # Directory where the k8s manifests are stored
 
@@ -12,6 +13,15 @@ docker build -t $FRONTEND_IMAGE ./frontend
 
 echo "Building backend Docker image..."
 docker build -t $BACKEND_IMAGE ./backend
+
+# Tag Docker images
+dokcer tag $FRONTEND_IMAGE $DOCKER_HUB_USERNAME/$FRONTEND_IMAGE
+dokcer tag $BACKEND_IMAGE $DOCKER_HUB_USERNAME/$BACKEND_IMAGE
+
+# Push Docker images to Docker Hub
+echo "Pushing frontend Docker image to Docker Hub..."
+docker push $DOCKER_HUB_USERNAME/$FRONTEND_IMAGE
+docker push $DOCKER_HUB_USERNAME/$BACKEND_IMAGE 
 
 # Kubernetes namespace creation (if needed)
 kubectl get namespace $NAMESPACE > /dev/null 2>&1
